7a20c29b85f9378dae564946d4f0b1fe4322f792
Rafael Gómez <rafitagd90@gmail.com>
2022-03-13 11:42:42 +0100

    Add project files.

---
+++ SampleWinUI3_AccessWebcam_4710/CameraCaptureUI.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.System;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SampleWinUI3_AccessWebcam_4710
+{
+    public class CameraCaptureUI
+    {
+        private LauncherOptions _launcherOptions;
+
+        public CameraCaptureUI(Window window)
+        {
+            var hndl = WindowNative.GetWindowHandle(window);
+
+            _launcherOptions = new LauncherOptions();
+            InitializeWithWindow.Initialize(_launcherOptions, hndl);
+
+            _launcherOptions.TreatAsUntrusted = false;
+            _launcherOptions.DisplayApplicationPicker = false;
+            _launcherOptions.TargetApplicationPackageFamilyName = "Microsoft.WindowsCamera_8wekyb3d8bbwe";
+        }
+
+        public async Task<StorageFile> CaptureFileAsync(CameraCaptureUIMode mode)
+        {
+            if (mode != CameraCaptureUIMode.Photo)
+            {
+                throw new NotImplementedException();
+            }
+
+            var currentAppData = ApplicationData.Current;
+            var tempLocation = currentAppData.TemporaryFolder;
+            var tempFileName = "CCapture.jpg";
+            var tempFile = await tempLocation.CreateFileAsync(tempFileName, CreationCollisionOption.GenerateUniqueName);
+            var token = Windows.ApplicationModel.DataTransfer.SharedStorageAccessManager.AddFile(tempFile);
+
+            var set = new ValueSet
+        {
+            { "MediaType", "photo" },
+            { "PhotoFileToken", token }
+        };
+
+            var uri = new Uri("microsoft.windows.camera.picker:");
+            var result = await Launcher.LaunchUriForResultsAsync(uri, _launcherOptions, set);
+            if (result.Status == LaunchUriStatus.Success)
+            {
+                return tempFile;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SampleWinUI3_AccessWebcam_4710/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SampleWinUI3_AccessWebcam_4710
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private Image image;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var captureUI = new CameraCaptureUI(this);
+            var file = await captureUI.CaptureFileAsync(Windows.Media.Capture.CameraCaptureUIMode.Photo);
+            if (file != null)
+            {
+                var bitmapImage = new BitmapImage()
+                {
+                    UriSource = new System.Uri(file.Path),
+                };
+                this.image.Source = bitmapImage;
+            }
+        }
+    }
+}
